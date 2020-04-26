792a117307d83e82426e2fac59ab0c0b6374333a
Inmo Jang <inmo.jang@manchester.ac.uk>
2020-04-26 21:11:42 +0900

    OpEn Python example

---
+++ tools_examples/OpEn/examples_python/solver_call.py
@@ -0,0 +1,27 @@
+import opengen as og
+
+mng = og.tcp.OptimizerTcpManager('python_build/paneltymethod_example')
+mng.start()
+
+pong = mng.ping()                 # check if the server is alive
+print(pong)
+response = mng.call([-10.0, 5])  # call the solver over TCP
+
+
+if response.is_ok():
+    # Solver returned a solution
+    solution_data = response.get()
+    u_star = solution_data.solution
+    exit_status = solution_data.exit_status
+    solver_time = solution_data.solve_time_ms
+    print("Solution: x = ", u_star)
+    print("Solver_time: = ", solver_time)
+else:
+    # Invocation failed - an error report is returned
+    solver_error = response.get()
+    error_code = solver_error.code
+    error_msg = solver_error.message
+
+
+
+mng.kill()
